4fcab69c356a0610d7f011399a2e5f2fc951fa66
NathanCerne <90366735+NathanCerne@users.noreply.github.com>
2024-04-27 10:28:51 -0500

    created Notification class

---
+++ LNNotification.java
@@ -0,0 +1,34 @@
+class LNNotification
+{
+
+    private int notifID;
+    private String bodyText;
+    private String titleText;
+    private LNAccount account;
+
+    public int getID()
+    {
+
+    }
+
+    public void detectActivity()
+    {
+
+    }
+
+    public String getTitle()
+    {
+
+    }
+
+    public String getBody()
+    {
+
+    }
+
+    public LNAccount getAccount()
+    {
+
+    }
+
+}
